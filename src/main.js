07fdea753b82625c005db21e07719c69cd4d1498
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-10-31 14:45:57 +0200

    hw_6

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
-type TreeNode = {
-    value: number;
-    child: TreeNode | null;
-};
-
-const buildTree = function (depth: number, startValue: number = 1): TreeNode | null {
+"use strict";
+var buildTree = function (depth, startValue) {
+    if (startValue === void 0) { startValue = 1; }
     if (depth <= 0) {
         return null;
     }
-
     return {
         value: startValue,
         child: buildTree(depth - 1, startValue + 1)
     };
 };
-
 console.log(buildTree(3));
-
